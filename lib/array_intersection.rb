ff56c74d00a0dd53da8461d090b87511c5cdee63
Kimberly-Fasbender <kimberly.fasbender@gmail.com>
2019-04-14 14:23:10 -0700

    elaborated on time complexity answer

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,6 +1,7 @@
 # Returns a new array to that contains elements in the intersection of the two input arrays
-# Time complexity: O(n+m)
-# Space complexity: O(n) if n < m, where n is the number of elements in the smallest array
+# Time complexity: O(n+m) - linear, where ne is the length of the smaller array, and m
+#                           is the length of the longer array
+# Space complexity: O(n) if n < m - linear, where n is the number of elements in the smallest array
 def intersection(array1, array2)
   if !array1 || !array2 || array1.length == 0 || array2.length == 0
     return []
